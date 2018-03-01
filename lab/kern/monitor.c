1f162bf11e31b1a054f1088ba9f117084860e47e
husterzx <husterzxh@foxmail.com>
2018-03-01 10:36:51 +0800

    finish lab1, mofify lab/kern/kdebug.c and lab/kern/monitor.c

--- lab/kern/monitor.c
+++ lab/kern/monitor.c
@@ -24,6 +24,7 @@ struct Command {
 static struct Command commands[] = {
 	{ "help", "Display this list of commands", mon_help },
 	{ "kerninfo", "Display information about the kernel", mon_kerninfo },
+	{ "backtrace", "Display information about the backtrace function", mon_backtrace },
 };
 
 /***** Implementations of basic kernel monitor commands *****/
@@ -58,6 +59,32 @@ int
 mon_backtrace(int argc, char **argv, struct Trapframe *tf)
 {
 	// Your code here.
+	int regebp = read_ebp();
+	int *ebp = (int *)regebp;
+
+	struct Eipdebuginfo info;
+	uintptr_t eip;
+	
+	cprintf("Stack backtrace:\n");
+	//If only we haven't pass the stack frame of i386_init
+	while ((int)ebp != 0x0)
+	{
+		cprintf("  ebp %08x", (int)ebp);
+		cprintf("  eip %08x", *(ebp+1));
+		cprintf("  args");
+		cprintf(" %08x", *(ebp+2));
+		cprintf(" %08x", *(ebp+3));
+		cprintf(" %08x", *(ebp+4));
+		cprintf(" %08x", *(ebp+5));
+		cprintf(" %08x\n", *(ebp+6));
+
+		eip = *(ebp+1);
+		debuginfo_eip(eip, &info);
+		cprintf("         %s:%d: %.*s+%d\n", info.eip_file, info.eip_line, info.eip_fn_namelen, info.eip_fn_name, eip - info.eip_fn_addr);
+
+		ebp = (int *)(*ebp);
+	}
+
 	return 0;
 }
 
